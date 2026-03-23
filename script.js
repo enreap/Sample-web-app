8ed2866f950b31e87df896b4b931e47ea11d83b0
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-03-23 13:44:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert() {
+    const message = document.getElementById("message");
+    alert("Hello! You clicked the button.");
+    message.textContent = "Button clicked! Deployment successful.";
+}
