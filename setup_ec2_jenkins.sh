b0126cc574a3626d06eb84ac53d14b2362542af2
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-03-24 15:59:08 +0530

    Update setup_ec2_jenkins.sh

--- setup_ec2_jenkins.sh
+++ setup_ec2_jenkins.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# -----------------------
-# Parameters
-# -----------------------
 REMOTE_USER=$1
 REMOTE_HOST=$2
-SSH_KEY_PATH=${3:-"$HOME/.ssh/id_ed25519"}  # default to ~/.ssh/id_rsa if not provided
-
-if [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_HOST" ]; then
-    echo "Usage: $0 <REMOTE_USER> <REMOTE_HOST> [SSH_KEY_PATH]"
-    exit 1
-fi
+SSH_KEY_PATH=${3:-"$HOME/.ssh/id_ed25519"}
 
 echo "Copying Jenkins SSH key to $REMOTE_USER@$REMOTE_HOST..."
 
+# Check key exists
+if [ ! -f "${SSH_KEY_PATH}.pub" ]; then
+    echo "ERROR: SSH key not found!"
+    exit 1
+fi
+
+# Copy key
 ssh-copy-id -i "${SSH_KEY_PATH}.pub" ${REMOTE_USER}@${REMOTE_HOST}
+
+# Check result
+if [ $? -ne 0 ]; then
+    echo "ERROR: ssh-copy-id failed"
+    exit 1
+fi
 
 echo "SSH key copied successfully!"
