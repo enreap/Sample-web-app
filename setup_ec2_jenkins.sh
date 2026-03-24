bbdd0e00f14dd74ec826c78f61d42f1f2e267345
enreap DevOps Team <184435543+enreap@users.noreply.github.com>
2026-03-24 15:49:59 +0530

    Update setup_ec2_jenkins.sh

--- setup_ec2_jenkins.sh
+++ setup_ec2_jenkins.sh
@@ -5,7 +5,7 @@
 # -----------------------
 REMOTE_USER=$1
 REMOTE_HOST=$2
-SSH_KEY_PATH=${3:-"$HOME/.ssh/id_rsa"}  # default to ~/.ssh/id_rsa if not provided
+SSH_KEY_PATH=${3:-"$HOME/.ssh/id_ed25519.pub"}  # default to ~/.ssh/id_rsa if not provided
 
 if [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_HOST" ]; then
     echo "Usage: $0 <REMOTE_USER> <REMOTE_HOST> [SSH_KEY_PATH]"
